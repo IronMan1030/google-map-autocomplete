6da633162c64b3f8580950ef2527f0add391fb26
yangting1030 <55540823+yangting1030@users.noreply.github.com>
2020-02-23 09:48:58 +0800

    first commit

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from "react";
+import "./App.css";
+import LocationSelect from "./components/LocationSelect";
+function App() {
+  return (
+    <div>
+      <LocationSelect />
+    </div>
+  );
+}
+
+export default App;
